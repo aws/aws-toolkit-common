e1ef953438e3c1f05087b63aed89ac12c304b43b
Hunter Werlla <werlla@amazon.com>
2020-01-15 09:13:38 -0800

    split up src to allow reuse of validation code

--- telemetry/vscode/scripts/setUpPackage.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-fs = require('fs-extra')
-
-fs.copySync('../telemetryDefinitions.json', './lib/telemetryDefinitions.json')
-fs.copySync('../telemetrySchema.json', './lib/telemetrySchema.json')
