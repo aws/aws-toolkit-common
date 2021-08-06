814e31135ed8eaae6e256a105e3c61f64537faab
Shruti Sinha <sinha.shruti085@gmail.com>
2021-08-06 15:59:00 -0700

    Support sending feedback with Visual Studio

---
+++ telemetry/csharp/AwsToolkit.Telemetry.Events/Core/Sentiment.cs
@@ -0,0 +1,18 @@
+﻿namespace Amazon.AwsToolkit.Telemetry.Events.Core
+{
+    /// <summary>
+    /// Constants used for properties of type Sentiment.
+    /// </summary>
+    public class Sentiment
+    {
+        public static readonly Sentiment Positive = new Sentiment("Positive");
+        public static readonly Sentiment Negative = new Sentiment("Negative");
+
+        public string Value { get; }
+
+        public Sentiment(string value)
+        {
+            Value = value;
+        }
+    }
+}
