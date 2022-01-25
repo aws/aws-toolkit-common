7aa0e0d3dad9c3ad2ef99fcdedfd570fcff3f4f0
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2022-01-25 14:00:40 -0700

    Disable additional publication created by `java-gradle-plugin` (#276)
    This was overwriting the intended publication

--- telemetry/jetbrains/build.gradle.kts
+++ telemetry/jetbrains/build.gradle.kts
@@ -128,6 +128,11 @@ publishing {
     }
 }
 
+// Disables the creation of an automatic publishing configuration
+// This is because `kotlin-dsl` pulls in `java-gradle-plugin` which generates a new publication automatically
+// We don't want to do two publications (this will clobber the first)
+gradlePlugin { setAutomatedPublishing(false) }
+
 signing {
     if (project.hasProperty("signing.keyId")
         && project.hasProperty("signing.password")
