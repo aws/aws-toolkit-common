7e99df2b5481cdac31dc1e47f71bc36e140e07f8
Joshua Pinkney <jpink@amazon.com>
2023-01-16 00:14:21 -0500

    Initialize language server

---
+++ README.md
@@ -0,0 +1,16 @@
+# AWS Documents Language Server
+
+Language server for AWS File types
+
+## Functionality
+
+This Language Server currently supports completion, hover, diagnostics for the following file types:
+- Buildspec
+
+## Structure
+
+```
+.
+── src
+    └── server.ts // Language Server entry point
+```
---
+++ src/server.ts
@@ -0,0 +1,111 @@
+import {
+    createConnection,
+    TextDocuments,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult
+} from 'vscode-languageserver/node';
+
+import {
+    TextDocument
+} from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    const capabilities = params.capabilities;
+
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Full,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: false
+            }
+        }
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true
+            }
+        };
+    }
+    return result;
+});
+
+connection.onInitialized(() => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(_event => {
+            connection.console.log('Workspace folder change event received.');
+        });
+    }
+});
+
+connection.onDidChangeConfiguration(change => {
+    // Revalidate all open text documents
+    documents.all().forEach(validateTextDocument);
+});
+
+// Only keep settings for open documents
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+documents.onDidClose(e => { });
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+    validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics: [] });
+}
+
+connection.onDidChangeWatchedFiles(_change => {
+    connection.console.log('We received an file change event');
+});
+
+connection.onCompletion(
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+        // TODO show buildspec completions
+        return [];
+    }
+);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
