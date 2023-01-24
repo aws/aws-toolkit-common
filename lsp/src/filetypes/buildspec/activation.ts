b3338c6b541f106fb38fa64a6a0366f0f5b14da7
Nikolas Komonen <nkomonen@amazon.com>
2023-01-24 12:10:36 -0500

    chore: buildspec match name variations
    This commit broadens which buildspec
    filenames will be considered a match
    for the LS to provide buildspec support.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- lsp/src/filetypes/buildspec/activation.ts
+++ lsp/src/filetypes/buildspec/activation.ts
@@ -5,17 +5,18 @@ import { BackendService } from '../../service'
 import { service } from './service'
 
 const SCHEMA_URL = 'https://d3rrggjwfhwld2.cloudfront.net/CodeBuild/buildspec/buildspec-standalone.schema.json'
+const BUILDSPEC_FILE_NAMES = ['build', 'buildspec'].map(name => [`${name}.yml`, `${name}.yaml`]).flat()
 
 export function activate(): RegistryItem {
     return {
         matches(uri: URI, contents: TextDocument) {
-            return uri.endsWith('build.yml') || uri.endsWith('build.yaml')
+            return BUILDSPEC_FILE_NAMES.some(accepted => uri.endsWith(accepted))
         },
         onMatch() {
             BackendService.getInstance().yaml.configure({
                 schemas: [
                     {
-                        fileMatch: ['build.yml', 'build.yaml'],
+                        fileMatch: BUILDSPEC_FILE_NAMES,
                         uri: SCHEMA_URL
                     }
                 ],
