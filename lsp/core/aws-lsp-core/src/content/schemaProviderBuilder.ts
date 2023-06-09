c1970408dba0bd481c3cc711f2b198d441044d98
Christopher Christou <chrstou@amazon.com>
2023-06-09 16:44:27 -0700

    Comment

--- lsp/core/aws-lsp-core/src/content/schemaProviderBuilder.ts
+++ lsp/core/aws-lsp-core/src/content/schemaProviderBuilder.ts
@@ -2,6 +2,12 @@ import { URI } from 'vscode-uri'
 import { ContentRequestMiddleware, ContentRequestMiddlewareDelegate } from './contentRequestMiddleware'
 import { SchemaProvider } from './schemaProvider'
 
+/**
+ * Creates a middleware pipeline for resolving requests for a schema.
+ * Middleware handlers will be called in the order they are added, until the
+ * first handler returns a response. The handlers that were called, will have
+ * a chance to handle/process the response (in reverse order).
+ */
 export class SchemaProviderBuilder {
     private readonly handlers: ContentRequestMiddleware[] = []
 
