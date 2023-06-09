10bdfef99231b9d9f093f1eea6709c8d81b7979a
Christopher Christou <chrstou@amazon.com>
2023-06-09 16:56:55 -0700

    Remove comments

--- lsp/core/aws-lsp-core/src/content/handlers/httpHandler.ts
+++ lsp/core/aws-lsp-core/src/content/handlers/httpHandler.ts
@@ -1,5 +1,5 @@
 import { URI } from 'vscode-uri'
-import { DefaultHttpRequester, HttpRequestHeaders, HttpRequester, getETag } from '../../http/requesters'
+import { DefaultHttpRequester, HttpRequester, getETag } from '../../http/requesters'
 import { ContentRequestMiddleware, ContentRequestMiddlewareDelegate } from '../contentRequestMiddleware'
 import { ContentRequestResponse } from '../contentRequestResponse'
 
@@ -9,14 +9,9 @@ export class HttpHandler implements ContentRequestMiddleware {
     /**
      * Downloads the content of a HTTP uri.
      *
-     * Additionally if an eTag is provided it will use it in the request.
-     * In the scenario the eTag matches the destinations, the content
-     * returned will be undefined.
-     *
-     * In any other scenario an error will be thrown.
      * @param uri
      * @param eTag
-     * @returns text content and eTag if it exists, on undefined content it is an eTag match
+     * @returns text content and eTag if it exists
      */
     async get(uri: URI, next: ContentRequestMiddlewareDelegate): Promise<ContentRequestResponse> {
         if (!uri.scheme.startsWith('http')) {
@@ -24,29 +19,8 @@ export class HttpHandler implements ContentRequestMiddleware {
             return next(uri)
         }
 
-        let headers: HttpRequestHeaders = {}
-
-        // if (eTag !== undefined) {
-        //     // 'If-None-Match' Header induces 304 status if ETag value matches the remote's.
-        //     // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
-        //     headers = { 'If-None-Match': eTag }
-        // }
-
-        // Request uri data
-        try {
-            const response = await this.httpContentDownloader.request(uri.toString(), { headers })
-            const latestETag = getETag(response.headers)
-            return { content: response.responseText, eTag: latestETag }
-        } catch (err) {
-            // if ((err as HttpResponse).status == 304) {
-            //     // The current http request library throws an error on,
-            //     // I'm assuming non 200 status codes, so we need to catch this
-            //     // case. If we can disable the exception this try/catch can be removed.
-
-            //     // Requested data matches our cache.
-            //     return { content: undefined, eTag }
-            // }
-            throw err
-        }
+        const response = await this.httpContentDownloader.request(uri.toString(), {})
+        const latestETag = getETag(response.headers)
+        return { content: response.responseText, eTag: latestETag }
     }
 }
