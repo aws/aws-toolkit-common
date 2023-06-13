e95119c4f70d3ee189e813534ad1c3b7c58ae563
Christopher Christou <chrstou@amazon.com>
2023-06-13 13:06:02 -0700

    Delete code that has been replaced by newer concept code

--- lsp/src/utils/json/service.ts
+++
@@ -1,6 +0,0 @@
-import { getLanguageService as getJsonLanguageService, LanguageService } from 'vscode-json-languageservice'
-
-export type JsonLanguageService = LanguageService
-export function createJsonLanguageService(): JsonLanguageService {
-    return getJsonLanguageService({})
-}
--- lsp/src/utils/parser/yaml.ts
+++
@@ -1,7 +0,0 @@
-import { parse as parseYamlImpl } from 'yaml-language-server/out/server/src/languageservice/parser/yamlParser07'
-import { YAMLDocument } from 'yaml-language-server/out/server/src/languageservice/parser/yaml-documents'
-import { TextDocument } from 'vscode-languageserver-textdocument'
-
-export function parseYaml(td: TextDocument): YAMLDocument {
-    return parseYamlImpl(td.getText(), undefined, td)
-}
