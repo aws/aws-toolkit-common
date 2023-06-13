11c68c6535e652ddf246589079fe1f9d29c596f3
Christopher Christou <chrstou@amazon.com>
2023-06-13 09:14:12 -0700

    Syntax change

--- lsp/core/aws-lsp-core/src/content/handlers/fileHandler.ts
+++ lsp/core/aws-lsp-core/src/content/handlers/fileHandler.ts
@@ -10,7 +10,7 @@ const readFile = promisify(fs.readFile)
 
 export class FileHandler implements ContentRequestMiddleware {
     async get(uri: URI, next: ContentRequestMiddlewareDelegate): Promise<ContentRequestResponse> {
-        if (!(uri.scheme == 'file')) {
+        if (uri.scheme !== 'file') {
             // we only know how to handle file requests. Go to the next handler.
             return next(uri)
         }
