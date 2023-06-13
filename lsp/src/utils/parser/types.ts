0a3fd587dde8603b970f1fd3c437366a28583b82
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-06-13 14:58:01 -0700

    Delete code that has been replaced by newer concept code (#512)
    This is part of a series of changes that establishes an LSP monorepo that we can perform experiments on.
    
    This change removes earlier concept code that has been replaced by newer concept code.

--- lsp/src/utils/parser/types.ts
+++
@@ -1,8 +0,0 @@
-import { TextDocument } from 'vscode-languageserver-textdocument'
-import { JSONDocument } from 'vscode-json-languageservice'
-import { YAMLDocument } from 'yaml-language-server/out/server/src/languageservice/parser/yaml-documents'
-
-export interface DocumentParserVisitor {
-    parseYAML(td: TextDocument): YAMLDocument
-    parseJSON(td: TextDocument): JSONDocument
-}
