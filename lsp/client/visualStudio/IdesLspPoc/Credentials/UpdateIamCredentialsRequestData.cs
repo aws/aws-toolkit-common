8320f2ab5ee32cf1897ec2e8e68e652a87e6e8fb
Christopher Christou <chrstou@amazon.com>
2023-08-09 10:33:46 -0700

    Update credentials encoding to use encrypted JWT Proofs of concept updated in: sample S3 language server, sample VS Code extension, sample VS extension

--- lsp/client/visualStudio/IdesLspPoc/Credentials/UpdateIamCredentialsRequestData.cs
+++ lsp/client/visualStudio/IdesLspPoc/Credentials/UpdateIamCredentialsRequestData.cs
@@ -1,9 +1,16 @@
-﻿namespace IdesLspPoc.Credentials
+﻿using Newtonsoft.Json;
+
+namespace IdesLspPoc.Credentials
 {
     public class UpdateIamCredentialsRequestData
     {
+        [JsonProperty("accessKeyId")]
         public string AccessKeyId;
+
+        [JsonProperty("secretAccessKey")]
         public string SecretAccessKey;
+
+        [JsonProperty("sessionToken")]
         public string SessionToken;
     }
 }
--- lsp/core/aws-lsp-core/src/credentials/encryption.ts
+++ lsp/core/aws-lsp-core/src/credentials/encryption.ts
@@ -2,6 +2,8 @@ export function shouldWaitForEncryptionKey(): boolean {
     return process.argv.some(arg => arg === '--pre-init-encryption')
 }
 
+export type CredentialsEncoding = 'JWT' | 'v3.local'
+
 /**
  * Servers expect IDE extensions to provide this payload as part of the startup sequence
  * if credentials are involved.
@@ -15,6 +17,10 @@ export type EncryptionInitialization = {
      * does not support, a fatal exception is thrown.
      */
     version: string
+    /**
+     * Indicates how credentials tokens will be encoded for this session.
+     */
+    mode: CredentialsEncoding
     /**
      * Base64 encoding of the encryption key to be used for the duration of this session.
      */
