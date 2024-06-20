f03c3ada74067c0dc9c9935e7b3706a625b88003
Dhruvi Gajjar <gajjardh@amazon.com>
2024-06-20 00:37:54 -0700

    test-fix

--- telemetry/jetbrains/build.gradle.kts
+++ telemetry/jetbrains/build.gradle.kts
@@ -153,7 +153,6 @@ nexusPublishing {
             // Using  token for authentication
             username.set(project.findProperty("ossrhUsername") as? String)
             password.set(project.findProperty("ossrhToken") as? String)
-        }
 
             // gotten using ./gradlew getStagingProfile with an older plugin (io.codearte.nexus-staging)
             stagingProfileId.set("29b8dd754a6907")
