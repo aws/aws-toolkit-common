954ffbd0259f257360893069bfd00df64a7e7404
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2024-07-12 13:02:49 -0700

    Apply CultureInvariant string conversion to more numeric metadata types (#781)
    ## Problem
    
    Most metadata is emitted using code that is auto-generated. There are times where we want to emit metadata from a generic source (like a deserialized JSON payload). These fields are treated as generic objects, which means numeric values get converted to strings using the user's locale settings. We have overloaded methods for certain types, which handle culture invariant conversion, but these fields are routed to the `object` overload. We want these to be culture invariant.
    
    ## Solution
    
    Add an overload that can be explicitly used, which routes the metadata to their type-specific handler.
    
    This change also converts the one test class assertions to use FluentAssertions.

--- telemetry/csharp/AwsToolkit.Telemetry.Events.Tests/Core/MetricDatumExtensionMethodsTests.cs
+++ telemetry/csharp/AwsToolkit.Telemetry.Events.Tests/Core/MetricDatumExtensionMethodsTests.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using Amazon.AwsToolkit.Telemetry.Events.Core;
+﻿using Amazon.AwsToolkit.Telemetry.Events.Core;
+using FluentAssertions;
+using System;
+using System.Globalization;
+using System.Threading;
 using Xunit;
 
 namespace Amazon.AwsToolkit.Telemetry.Events.Tests.Core
@@ -13,45 +16,100 @@ namespace Amazon.AwsToolkit.Telemetry.Events.Tests.Core
         public void AddMetadata_NonBlankEntries()
         {
             _sut.AddMetadata(Key, "hello");
-            Assert.Equal("hello", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("hello");
 
             _sut.AddMetadata(Key, 123);
-            Assert.Equal("123", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("123");
 
             _sut.AddMetadata(Key, 123.456);
-            Assert.Equal("123.456", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("123.456");
 
             _sut.AddMetadata(Key, 88.88d);
-            Assert.Equal("88.88", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("88.88");
 
             _sut.AddMetadata(Key, true);
-            Assert.Equal("true", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("true");
 
             _sut.AddMetadata(Key, false);
-            Assert.Equal("false", _sut.Metadata[Key]);
+            _sut.Metadata[Key].Should().Be("false");
+        }
+
+        public static TheoryData<object, string> CreateNumericEntries()
+        {
+            return new TheoryData<object, string>
+            {
+                { (int) 123_456, "123456" },
+                { (uint) 123_456, "123456" },
+                { (Int16) 12_345, "12345" },
+                { (Int32) 123_456, "123456" },
+                { (Int64) 123_456, "123456" },
+                { (UInt16) 12_345 , "12345" },
+                { (UInt32) 123_456, "123456" },
+                { (UInt64) 123_456, "123456" },
+                { (short) 12_345, "12345" },
+                { (ushort) 12_345, "12345" },
+                { (long) 123_456, "123456" },
+                { (ulong) 123_456, "123456" },
+                { (float) 1_234.56, "1234.56" },
+                { (float) 1.234_5, "1.2345" },
+                { (decimal) 123_456.789_012, "123456.789012" },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateNumericEntries))]
+        public void AddMetadata_DetectPrimitiveType_NumericEntries(object numericValue, string expectedValue)
+        {
+            _sut.AddMetadata(Key, numericValue, detectPrimitiveType: true);
+            _sut.Metadata[Key].Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateNumericEntries))]
+        public void AddMetadata_DetectPrimitiveType_NumericEntries_Us(object numericValue, string expectedValue)
+        {
+            // Use a locale known to have decimal separators
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            _sut.AddMetadata(Key, numericValue, detectPrimitiveType: true);
+            _sut.Metadata[Key].Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateNumericEntries))]
+        public void AddMetadata_DetectPrimitiveType_NumericEntries_CommaSeparatorLocale(object numericValue, string expectedValue)
+        {
+            // Set the local to use comma separators
+            var commaLocale = new CultureInfo("en-US");
+            commaLocale.NumberFormat.NumberDecimalSeparator = ",";
+
+            Thread.CurrentThread.CurrentCulture = commaLocale;
+
+            _sut.AddMetadata(Key, numericValue, detectPrimitiveType: true);
+            _sut.Metadata[Key].Should().Be(expectedValue);
         }
 
         [Fact]
         public void AddMetadata_BlankEntries()
         {
             _sut.AddMetadata(Key, (object) null);
-            Assert.False(_sut.Metadata.ContainsKey(Key));
+            _sut.Metadata.Should().NotContainKey(Key);
 
             _sut.AddMetadata(Key, (string) null);
-            Assert.False(_sut.Metadata.ContainsKey(Key));
+            _sut.Metadata.Should().NotContainKey(Key);
 
             _sut.AddMetadata(Key, string.Empty);
-            Assert.False(_sut.Metadata.ContainsKey(Key));
+            _sut.Metadata.Should().NotContainKey(Key);
 
             _sut.AddMetadata(Key, "   ");
-            Assert.False(_sut.Metadata.ContainsKey(Key));
+            _sut.Metadata.Should().NotContainKey(Key);
         }
 
         [Fact]
         public void InvokeTransform_Null()
         {
             var updatedDatum = _sut.InvokeTransform(null);
-            Assert.Equal(_sut, updatedDatum);
+            _sut.Should().BeSameAs(updatedDatum);
         }
 
         [Fact]
@@ -63,7 +121,7 @@ namespace Amazon.AwsToolkit.Telemetry.Events.Tests.Core
             }
 
             var updatedDatum = _sut.InvokeTransform(TransformFunction);
-            Assert.NotNull(updatedDatum);
+            updatedDatum.Should().NotBeNull();
         }
 
         [Fact]
@@ -76,7 +134,7 @@ namespace Amazon.AwsToolkit.Telemetry.Events.Tests.Core
             }
 
             var updatedDatum = _sut.InvokeTransform(TransformFunction);
-            Assert.Equal("hello", updatedDatum.Metadata[Key]);
+            updatedDatum.Metadata[Key].Should().Be("hello");
         }
     }
 }
