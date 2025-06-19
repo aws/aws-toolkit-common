376d23caa43b666ede056d8e5352c1de260f3c3c
Bryce Ito <bryceito@amazon.com>
2025-06-19 09:03:28 -0700

    Migrate to Maven Central Publisher Portal

--- telemetry/jetbrains/build.gradle.kts
+++ telemetry/jetbrains/build.gradle.kts
@@ -170,8 +170,8 @@ nexusPublishing {
 
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://aws.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://aws.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(project.findProperty("ossrhUsername") as? String)
             password.set(project.findProperty("ossrhPassword") as? String)
 
