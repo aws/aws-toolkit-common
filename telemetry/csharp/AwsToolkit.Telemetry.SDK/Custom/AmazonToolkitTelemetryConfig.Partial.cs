c834f7fb684794d0597a74f0e1c6dfecda6e978a
Shruti Sinha <shrsinh@amazon.com>
2025-09-12 16:14:07 -0700

    Use a partial config file to address the missing DetermineServiceEndpoint method

---
+++ telemetry/csharp/AwsToolkit.Telemetry.SDK/Custom/AmazonToolkitTelemetryConfig.Partial.cs
@@ -0,0 +1,45 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Amazon.Runtime;
+using Amazon.Runtime.Endpoints;
+using Amazon.Runtime.Internal;
+
+namespace Amazon.ToolkitTelemetry
+{
+    /// <summary>
+    /// Configuration for accessing Amazon ToolkitTelemetry service - Custom partial class
+    /// </summary>
+    public partial class AmazonToolkitTelemetryConfig
+    {
+        /// <summary>
+        /// Returns the endpoint that will be used for a particular request.
+        /// </summary>
+        /// <param name="parameters">A Container class for parameters used for endpoint resolution.</param>
+        /// <returns>The resolved endpoint for the given request.</returns>
+        public override Amazon.Runtime.Endpoints.Endpoint DetermineServiceOperationEndpoint(ServiceOperationEndpointParameters parameters)
+        {
+            var request = new DefaultRequest(parameters.Request, ServiceId);
+            request.AlternateEndpoint = parameters.AlternateEndpoint;
+            
+            var requestContext = new RequestContext(false);
+            requestContext.ClientConfig = this;
+            requestContext.OriginalRequest = parameters.Request;
+            requestContext.Request = request;
+            var executionContext = new Amazon.Runtime.Internal.ExecutionContext(requestContext, null);
+            return new BaseEndpointResolver().GetEndpoint(executionContext);
+        }
+    }
+}
